cc5207a53d50f914b6d1fecc797b0170fde5c842
araiwa <naomi.araiwa@googlemail.com>
2023-05-05 11:09:48 +0200

    Brush up you code

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -2,11 +2,83 @@ package machine
 const val MILK:Int = 50
 const val BEANS:Int = 15
 const val WATER:Int = 200
+var machine: CoffeeMachine = CoffeeMachine()
 var available_water = 400
 var available_milk = 540
 var available_beans = 120
 var available_money = 550
 var available_cups = 9
+
+
+class CoffeeMachine{
+    enum class State{
+        ACTION, COFFEE, FILL_WATER, FILL_BEANS, FILL_MILK, FILL_CUPS
+    }
+
+    var currentState: State = State.ACTION
+    var machine_on: Boolean = true
+    private fun fill(userInput: String){
+        when(currentState) {
+            State.ACTION -> {
+                println("Write how many ml of water you want to add:")
+               currentState = State.FILL_WATER
+            }
+            State.FILL_WATER -> {
+                available_water += userInput.toInt()
+                println("Write how many ml of milk you want to add:")
+                currentState = State.FILL_MILK
+            }
+            State.FILL_MILK -> {
+                available_milk += userInput.toInt()
+                println("Write how many grams of coffee beans you want to add:")
+                currentState = State.FILL_BEANS
+            }
+            State.FILL_BEANS -> {
+                available_beans += userInput.toInt()
+                println("Write how many grams of coffee beans you want to add:")
+                currentState = State.FILL_CUPS
+            }
+            State.FILL_BEANS -> {
+                available_beans += userInput.toInt()
+                println("Write how many grams of coffee beans you want to add:")
+                currentState = State.FILL_CUPS
+            }
+            State.FILL_CUPS -> {
+                available_cups += userInput.toInt()
+                currentState = State.ACTION
+                println("Write action (buy, fill, take, remaining, exit):")
+            }
+        }
+    }
+
+
+
+    fun working(userInput: String){
+        if (currentState == State.ACTION){
+            when (userInput){
+                "buy" -> {println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+                        currentState = State.COFFEE}
+                "fill" -> fill(userInput)
+                "take" -> take()
+                "remaining" -> remaining()
+                "exit" -> machine_on = false
+            }
+        }else if(currentState == State.COFFEE){
+            when(userInput){
+                "1" -> makeDrink(250, 16, 4, 0)
+                "2" -> makeDrink(350, 20, 7, 75)
+                "3" -> makeDrink(200, 12, 6, 100)
+                "back" -> currentState = State.ACTION
+            }
+            currentState = State.ACTION
+        }else{
+            fill(userInput)
+        }
+
+
+    }
+
+}
 
 fun remaining(){
     println("""
@@ -17,6 +89,7 @@ fun remaining(){
         ${available_cups} disposable cups
         ${'$'}${available_money} of money
     """.trimIndent())
+    println("Write action (buy, fill, take, remaining, exit):")
 }
 fun makeDrink(water: Int, beans: Int, money: Int, milk: Int){
     if(water > available_water) println("Sorry, not enough water!")
@@ -31,45 +104,22 @@ fun makeDrink(water: Int, beans: Int, money: Int, milk: Int){
         available_milk-=milk
         available_money+=money
     }
+    println("Write action (buy, fill, take, remaining, exit):")
 
 }
-fun buy(){
-    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-    when(readln()){
-        "1" -> makeDrink(250, 16, 4, 0)
-        "2" -> makeDrink(350, 20, 7, 75)
-        "3" -> makeDrink(200, 12, 6, 100)
-        "back" -> return
-    }
 
 
-}
-fun fill(){
-    println("Write how many ml of water you want to add:")
-    available_water += readln().toInt()
-    println("Write how many ml of milk you want to add:")
-    available_milk += readln().toInt()
-    println("Write how many grams of coffee beans you want to add:")
-    available_beans += readln().toInt()
-    println("Write how many disposable cups you want to add:")
-    available_cups += readln().toInt()
-}
+
+
 fun take(){
     println("I gave you $('$')${available_money}")
     available_money = 0
 }
 
 fun main() {
-    while(true){
-        println("Write action (buy, fill, take, remaining, exit):")
-        when(readln()){
-            "buy" -> buy()
-            "fill" -> fill()
-            "take" -> take()
-            "remaining" -> remaining()
-            "exit" -> break
-        }
-
+    println("Write action (buy, fill, take, remaining, exit):")
+    while(machine.machine_on){
+        machine.working(readln())
     }
 
 
---
+++ Topics/Declaring classes/New planet/src/Main.kt
@@ -0,0 +1,7 @@
+// write your code here
+class Planet(){
+    var star: String = "Unknown Star"
+    var mass: Double = 1.0
+    var numberOfSatellites: Int = 1
+
+}
